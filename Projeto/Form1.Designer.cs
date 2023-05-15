f1821149a17e641111e689f8cfa22aa4c3daf0c7
CarlosMachad0 <107853800+CarlosMachad0@users.noreply.github.com>
2023-05-15 16:41:07 +0100

    Adicionar arquivos de projeto.

---
+++ Projeto/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(314, 12);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(923, 600);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Procurar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "PrintScreen";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1249, 638);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Projeto/Form1.cs
@@ -0,0 +1,79 @@
+﻿
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.PowerPoint;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+using Application = System.Windows.Forms.Application;
+
+namespace Projeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicializa o controle WebView2
+            webView21.CreationProperties = new CoreWebView2CreationProperties();
+            webView21.CreationProperties.UserDataFolder = Application.UserAppDataPath;
+            webView21.CoreWebView2InitializationCompleted += WebView21_CoreWebView2InitializationCompleted;
+            webView21.EnsureCoreWebView2Async();
+
+            // Manipula o evento Click do botão "Ir"
+            button1.Click += Button1_Click;
+
+            // Manipula o evento Click do botão "Capturar tela"
+            button2.Click += Button2_Click;
+        }
+
+        private void WebView21_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            // Carrega a página da web padrão no WebView2
+            webView21.CoreWebView2.Navigate("https://www.example.com");
+            webView21.CoreWebView2.SourceChanged += CoreWebView2_SourceChanged;
+        }
+
+        private void CoreWebView2_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            // Exibe a URL da página carregada no TextBox
+            textBox1.Text = webView21.CoreWebView2.Source;
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            // Verifica se a URL digitada é válida
+            if (Uri.TryCreate(textBox1.Text, UriKind.Absolute, out Uri uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+            {
+                // Navega para a URL digitada no TextBox
+                webView21.CoreWebView2.Navigate(textBox1.Text);
+            }
+            else
+            {
+                // Exibe uma mensagem de erro se a URL não for válida
+                MessageBox.Show("URL inválida. Por favor, insira uma URL válida começando com 'http://' ou 'https://'.");
+            }
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            // Captura um screenshot da WebView2 e salva em um arquivo
+            using (var fileStream = File.Create(@"C:\DEVSCOPE\Projeto\Projeto\bin\Debug\testeImg.png"))
+            {
+                await webView21.CoreWebView2.CapturePreviewAsync(CoreWebView2CapturePreviewImageFormat.Png, fileStream);
+            }
+            MessageBox.Show("Captura de tela salva como 'testeImg.png'.");
+        }
+    }
+}
